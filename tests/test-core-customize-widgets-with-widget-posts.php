3d75e534c708414dbd7bf573b1ecf5a9170f7649
Weston Ruter <weston@xwp.co>
2015-05-22 23:54:37 -0700

    Add Efficient_Multidimensional_Setting_Sanitizing to Core test

--- tests/test-core-customize-widgets-with-widget-posts.php
+++ tests/test-core-customize-widgets-with-widget-posts.php
@@ -23,6 +23,7 @@ class Test_Core_Customize_Widgets_With_Widget_Posts extends \Tests_WP_Customize_
 		$this->plugin->widget_factory = $wp_widget_factory;
 		$this->plugin->widget_number_incrementing = new Widget_Number_Incrementing( $this->plugin );
 		$this->plugin->widget_posts = new Widget_Posts( $this->plugin );
+		$this->plugin->efficient_multidimensional_setting_sanitizing = new Efficient_Multidimensional_Setting_Sanitizing( $this->plugin, $this->manager );
 
 		$widgets_init_hook = 'widgets_init';
 		$callable = array( $wp_widget_factory, '_register_widgets' );
@@ -37,6 +38,12 @@ class Test_Core_Customize_Widgets_With_Widget_Posts extends \Tests_WP_Customize_
 
 		$this->plugin->widget_posts->migrate_widgets_from_options();
 		$this->plugin->widget_posts->init();
+	}
+
+	function test_register_settings() {
+		parent::test_register_settings();
+		$this->assertInstanceOf( __NAMESPACE__ . '\\WP_Customize_Widget_Setting', $this->manager->get_setting( 'widget_categories[2]' ) );
+		$this->assertEquals( 'widget', $this->manager->get_setting( 'widget_categories[2]' )->type );
 	}
 
 }
