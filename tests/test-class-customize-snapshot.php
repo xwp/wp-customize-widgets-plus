64f95195f5c05f63cd766e1b2752b85403934b32
Derek Herman <derek@valendesigns.com>
2015-10-21 03:29:00 -0700

    Add more incomplete tests

---
+++ tests/test-class-customize-snapshot.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace CustomizeWidgetsPlus;
+
+class Test_Customize_Snapshot extends Base_Test_Case {
+
+	/**
+	 * @see Customize_Snapshot::__construct()
+	 */
+	function test_construct() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::populate_customized_post_var()
+	 */
+	function test_populate_customized_post_var() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::generate_uuid()
+	 */
+	function test_generate_uuid() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::is_valid_uuid()
+	 */
+	function test_is_valid_uuid() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::uuid()
+	 */
+	function test_uuid() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::manager()
+	 */
+	function test_manager() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::is_preview()
+	 */
+	function test_is_preview() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::post()
+	 */
+	function test_post() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::_override_wp_query_is_single()
+	 */
+	function test_override_wp_query_is_single() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::get()
+	 */
+	function test_get() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::data()
+	 */
+	function test_data() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::settings()
+	 */
+	function test_settings() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::status()
+	 */
+	function test_status() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::set()
+	 */
+	function test_set() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::saved()
+	 */
+	function test_saved() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot::save()
+	 */
+	function test_save() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+
+}
