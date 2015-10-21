f83fe32b253ccebc5121daa0a6b96fe49a15792b
Derek Herman <derek@valendesigns.com>
2015-10-21 03:22:10 -0700

    Add incomplete tests

---
+++ tests/test-class-customize-snapshot-manager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CustomizeWidgetsPlus;
+
+class Test_Customize_Snapshot_Manager extends Base_Test_Case {
+
+	/**
+	 * @see Customize_Snapshot_Manager::__construct()
+	 */
+	function test_construct() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot_Manager::store_post_data()
+	 */
+	function test_store_post_data() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot_Manager::create_post_type()
+	 */
+	function test_create_post_type() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot_Manager::enqueue_scripts()
+	 */
+	function test_register_scripts() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot_Manager::update_snapshot()
+	 */
+	function test_update_snapshot() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @see Customize_Snapshot_Manager::preview()
+	 */
+	function test_preview() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+}
